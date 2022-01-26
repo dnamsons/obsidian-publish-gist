152bcca46cec025b5ef2a8a0aa510e239dc514c2
Davis Namsons <davisnamsons@gmail.com>
2022-01-26 15:35:29 +0200

    Implement an obsidian command to publish note to a gist

--- main.ts
+++ main.ts
@@ -1,137 +1,115 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian'
+import { Octokit } from '@octokit/rest'
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface PluginSettings {
+	token: string
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PluginSettings = {
+	token: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PublishGistPlugin extends Plugin {
+	settings: PluginSettings
+	githubInterface?: Octokit
+	authenticated: boolean = false
 
 	async onload() {
-		await this.loadSettings();
+		await this.loadSettings()
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		if (this.settings.token !== '') {
+			this.githubInterface = new Octokit({ auth: this.settings.token })
+		}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.addCommand({
+			id: 'publish-gist',
+			name: 'Publish file to Github gist',
+			editorCallback: async (editor) => {
+				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView)
+				if (!!this.githubInterface) {
+					const file = markdownView.file
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+					let content = await this.app.vault.read(file)
+
+					const frontmatter = this.app.metadataCache.getFileCache(file)?.frontmatter
+
+					let result
+
+					if (frontmatter && frontmatter.gist_id) {
+						result = await this.githubInterface.rest.gists.update({
+							gist_id: frontmatter.gist_id,
+							files: { [file.name]: { content } },
+							public: false
+						})
+					} else {
+						result = await this.githubInterface.rest.gists.create({
+							files: { [file.name]: { content } },
+							public: false
+						})
+
+						const gistId = result.data.id
+
+						if (frontmatter) {
+							let frontmatterEnd = frontmatter.position.end.line
+							let lines = content.split('\n')
+							lines.splice(frontmatterEnd, 0, `gist_id: \"${gistId}\"`)
+
+							editor.setValue(lines.join('\n'))
+						} else {
+							editor.setValue(`---\ngist_id: \"${gistId}\"\n---\n\n${content}`)
+						}
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					new Notice('Created successfully!')
+				} else {
+					new Notice('No Github token')
 				}
 			}
-		});
+		})
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new GistSyncSettingTab(this.app, this))
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
+		await this.saveData(this.settings)
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class GistSyncSettingTab extends PluginSettingTab {
+	plugin: PublishGistPlugin
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	constructor(app: App, plugin: PublishGistPlugin) {
+		super(app, plugin)
+		this.plugin = plugin
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this
 
-		containerEl.empty();
+		containerEl.empty()
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		const span = containerEl.createSpan()
+		containerEl.createEl('p', {
+			text: 'To use this plugin, you need to create a Github API token',
+			parent: span
+		})
+		containerEl.createEl('a', {
+			text: 'Create a Github API token.',
+			href: 'https://github.com/settings/tokens/new',
+			parent: span
+		})
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		new Setting(containerEl).setName('Github access token').addText((text) =>
+			text.setValue(this.plugin.settings.token).onChange(async (value) => {
+				this.plugin.settings.token = value
+				await this.plugin.saveSettings()
+			})
+		)
 	}
 }
